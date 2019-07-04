ad4399d731c4b73e3eed254bfe7a08133e32e4e4
Frari <francescoriccardi86@gmail.com>
2019-07-04 17:43:45 +0200

    problema con css

--- resources/views/homeBoolean.blade.php
+++ resources/views/homeBoolean.blade.php
@@ -1,1 +1,12 @@
-<h1>ciao</h1>
+<!-- estensione verso la pagina dove ci sono i layout -->
+@extends('layouts.base')
+
+<!-- layout hanbar -->
+@section('navbar')
+  
+@endsection
+
+<!-- layout footer -->
+@section('footer')
+
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  </head>
+  <body>
+    <div class="navbar">
+      @yield('navbar')
+      <img src="https://www.boolean.careers/images/common/logo.png" alt="logo">
+    </div>
+  </body>
+</html>
